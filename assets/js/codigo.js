de6d1655cfbc9e26c1d9d11085724ca403e35d93
claudia <claudia_figueroai@hotmail.com>
2021-09-29 17:57:29 -0300

    se añade efecto jquery al hacer click en h2 que cambien de color

---
+++ assets/js/codigo.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    //al pinchar en quienes somos se cambiaran el fondo de los 3 h2 que existen
+    $("#quienes-somos").on("click", function(){
+        $("h2").addClass("bg-warning");
+    });
+
+    
+
+})
+
+
+
